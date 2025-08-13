b415e4252d68774fa72c2731d4027fa6bc6b56d4
nickrafael9-maker <nickrafael9@gmail.com>
2025-08-12 22:23:48 -0400

    Add files via upload

---
+++ proyectos742/fundamentos/clase01.py
@@ -0,0 +1,36 @@
+print ("hola mundo desde python")
+
+
+#variable de tipo cadena        
+nombre = "Nick Rafael"
+
+#Variable de tipo entero
+edad = 24
+
+#ariable de tipo flotante 
+altura = 1.76
+
+#Variable de tipo booleano
+es_estudiante = True
+
+#imprimir las variables
+print("nombre",nombre)
+print("edad ",edad)
+print("altura",altura)
+print("es_estudiante",es_estudiante)
+
+print("Impresion moderna con f-strings")
+print("---------------------")
+print(f"Nombre:{nombre}")      
+print(f"Edad:{edad}")       
+print(f"Altura:{altura}")       
+print(f"Es_estudiante:{es_estudiante}")        
+
+
+
+
+
+
+
+
+
